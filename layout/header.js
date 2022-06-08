7971b4c36f1d31e52c171fac6785a18432615cfb
unknown <shyamattryy@gmail.com>
2022-06-08 16:23:41 +0530

    changes

---
+++ layout/header.js
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+function Footer2() {
+  return (
+    <>
+    {/* <!-- Footer --> */}
+    <footer className="site-header style-2">
+		
+		<div className="header-top" style={{"backgroundColor":"#012e6b", "padding":"10px 0px"}}>
+           
+				<div className="row">
+			
+                </div>
+            
+        </div>
+        {/* <!-- footer bottom part --> */}
+        <div className="header-bottom">
+            <section class="">
+                <div className="row align-items-center">
+                    <div className="col-lg-6 col-md-7 text-left"> 
+					<Image  src="/img/logo-1-1 (1).png" alt="Picture of the logo" width={160} height={80} />
+					</div>
+					 {/* <div className="col-lg-6 col-md-5 text-right">
+						<div><Link href="+91 8968140816">
+          <a>+91 8968140816</a>
+        </Link></div>
+		<div><Link href="+91 8968140816">
+          <a>+91 7982518911</a>
+        </Link></div>
+					</div> */}
+<div className="col-lg-6 col-md-5 text-right">
+<div className="header_boxstyle5">
+              <ul>
+                <li>
+                  <div className="image22">
+                    <Image src="/img/icons/icon.svg" alt="Picture of the logo" width={40} height={40} />
+                  </div>
+                </li>
+                <li>
+                  <div className="headerright">
+				  <div><Link href="+91 8968140816">
+          <a>+91 8968140816</a>
+        </Link></div>
+		<div><Link href="+91 9773864861">
+          <a>+91 9773864861</a>
+        </Link></div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+			</div>
+                </div>
+            </section>
+        </div>
+    </footer>
+    {/* <!-- Footer End --></footer> */}
+    </>
+  )
+}
+
+export default Footer2;
---
+++ pages/thanku.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image';
+function thanku() {
+  return (
+    <div className='thankupage'>
+<Image src="/img/Anatomy-of-the-Perfect-Thank-You-Page.png" className='imageth' alt="image" height={300} width={620} />
+    </div>
+  )
+}
+
+export default thanku;
