6f78f9850a9c28cdeb906d3c5b29d6b4c812c7ed
shyamattryy <shyamattryy.com>
2022-06-22 13:35:39 +0530

    4th Id add And Thank u page Changes

--- pages/thanku.js
+++ pages/thanku.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from "./../layout/header";
 import Image from 'next/image';
+import Link from 'next/link';
 function thanku() {
   return (
     <>  
     <Header />
       <div className='thankupage'>
      <Image src="/img/thankyou.jpg" className='imageth' alt="image" height={300} width={620} />
+     <div>
+      <p style={{color:'rgb(1, 46, 107)'}}>Our Representative Will Get Back to You Shortly</p>
+      <Link href="/"><a>Back to Home</a></Link>
+     </div>
     </div>
     </>
 
